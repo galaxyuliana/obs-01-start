632f294432a5d7e2399cc13a42219f5ea9494534
galaxyuliana <yulianashin613@gmail.com>
2022-10-12 23:04:41 -0700

    Added interval to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { interval } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +12,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    interval(1000).subscribe(count => {
+      console.log(count);
+    });
   }
 
 }
