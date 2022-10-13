e3a614a4ea0e87f1c8cc612cd38c99afeebdb402
galaxyuliana <yulianashin613@gmail.com>
2022-10-12 23:08:19 -0700

    Added subscription to unsubscribe interval

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    interval(1000).subscribe(count => {
+    this.firstObsSubscription = interval(1000).subscribe(count => {
       console.log(count);
     });
+  }
+
+  ngOnDestroy(): void {
+      this.firstObsSubscription.unsubscribe();
   }
 
 }
